30110cb72724e232b8b552ee6f2605f153183736
nDeepakDeepu <ndeepak.raikar@gmail.com>
2020-05-08 18:22:38 +0530

    Íntial commit

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = ({name,email,id,contact}) => {
+    // const{ name,email,id} = props; 
+     return (
+        <div className='tc bg-light-blue dib br2 pa2 ma2 grow bw4 shadow-5'>
+            {/* <h1>Robo friends</h1> */}
+            <img alt="robot"  src={`https://robohash.org/${id}?100x120`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+                <p>{contact}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/robot.js
@@ -0,0 +1,73 @@
+export const robots = [
+{
+    id : 1,
+    name : 'Q',
+    username  : 'lee',
+    contact :'12365'
+},
+
+{
+    id : 2,
+    name : 'W',
+    username  : 'goa',
+    contact :'1655'
+},
+
+{
+    id : 3,
+    name : 'E',
+    username  : 'kohli',
+    contact :'125514'
+},
+{
+    id : 4,
+    name : 'R',
+    username  : 'dhoni',
+    contact :'12365'
+}
+,
+{
+    id : 5,
+    name : 'Rdac',
+    username  : 'dfcefchoni',
+    contact :'12365we32r'
+}
+,
+{
+    id : 5,
+    name : 'dafcsdvR',
+    username  : 'dh3rroni',
+    contact :'12365fcsf'
+}
+,
+{
+    id : 6,
+    name : 'cvfbdgR',
+    username  : 'dhcscsdoni',
+    contact :'12365awe3'
+},
+{
+    id : 7,
+    name : 'R',
+    username  : 'dhoni',
+    contact :'12365'
+},
+{
+    id : 8,
+    name : 'efwfR',
+    username  : 'fewfewfdhoni',
+    contact :'12365234r'
+},
+{
+    id : 9,
+    name : 'ewfwR',
+    username  : 'ewfwefwedhoni',
+    contact :'123654ef'
+},
+{
+    id : 10,
+    name : 'ewfwR',
+    username  : 'fwefswdhoni',
+    contact :'12365fes'
+}
+]
