0d1079a450dccf9e8a470d62d5d17f4a7b48352a
nDeepakDeepu <ndeepak.raikar@gmail.com>
2020-05-11 18:38:09 +0530

    removed website

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,8 +10,6 @@ const Card = ({name,email,id,contact,street,phone,website,address}) => {
                 <h3>Name : {name}</h3>
                 <p> Email : {email}</p>
                 <p> phone : {phone}</p>
-                <p> street : {street} </p>
-                         
             </div>
         </div>
     );
